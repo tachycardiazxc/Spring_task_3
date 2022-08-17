9e44728d7a8d0c48ba19147a8eda3edeeb31380c
devourmealive <foxmanlisichka@yandex.ru>
2022-08-17 15:31:39 +0300

    finished

--- src/main/java/koschei/models/Island2.java
+++ src/main/java/koschei/models/Island2.java
@@ -1,7 +1,10 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class Island2 {
 
+    @Autowired
     private Wood3 wood;
 
     public Island2(Wood3 wood) {
